511ece16a865a996b05af6ffc4c85adb1c06821f
Ganesh-Sharmaz <shashanklhr@gmail.com>
2024-02-22 09:54:13 +0530

    update

---
+++ navbar/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className=' bg-red'>
+        <div className=''>Title</div>
+        <div></div>
+        <div>icons</div>
+      </div>
+    </>
+  )
+}
+
+export default App
